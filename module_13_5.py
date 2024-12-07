1e5a3206054d826c05fe55834ceba6f06cc31428
Sachvk <serg22011985@gmail.com>
2024-12-07 16:43:16 +0300

    Add files via upload

---
+++ module_13_5.py
@@ -0,0 +1,63 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+api = "7603802467:AAGbZpnyscpFeBfB7fyStU2kfpd7gd5uq4w"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+kb = ReplyKeyboardMarkup()
+button1 = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+kb.add(button1)
+kb.add(button2)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text='Рассчитать')
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой вec:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def send_calories(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    result = (10 * int(data['age']) + 6.25 * int(data['growth']) - 5 * int(data['weight']) + 5) * 1.55
+    await message.answer(f'Ваша норма калорий:{result}')
+    await state.finish()
+
+
+@dp.message_handler()
+async def all_massages(message):
+    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+executor.start_polling(dp, skip_updates=True)
